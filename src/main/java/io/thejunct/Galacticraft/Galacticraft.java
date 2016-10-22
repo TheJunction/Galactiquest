ac10f9090357749209fb850006739a07b53eaac7
David Shen <me@pantherman594.com>
2016-10-22 14:48:52 -0400

    Initial commit (setup)

---
+++ src/main/java/io/thejunct/Galacticraft/Galacticraft.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 The Junction Network. All Rights Reserved.
+ * Created by PantherMan594.
+ */
+
+package io.thejunct.Galacticraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by david on 10/22.
+ *
+ * @author david
+ */
+public class Galacticraft extends JavaPlugin {
+
+}
